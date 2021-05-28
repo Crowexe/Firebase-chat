8b0cd7c7a719adee963a8d95a9d3f0e095ddd7ba
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2021-05-27 22:18:25 -0500

    basic fuctions added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Channel from "./components/Channel";
+import { useUser } from "./context/user";
+
+const App = () => {
+  const { user, login } = useUser();
+  
+  return (
+    <div>
+      {user ? <Channel /> : <button onClick={login}>login with Google</button>}
+    </div>
+  )
+}
+
+
+export default App;
